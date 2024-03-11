7e5d1e29b6413be9d711fdb8f247d7b7ae39b5be
Sachin Tekchandani <sachin6291@gmail.com>
2024-03-11 00:31:44 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import styled from '@emotion/styled'
+
+const Heading = styled.h1`
+  font-family: Lato, sans-serif;
+  color:#FFF;
+`
+
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <Heading>from App</Heading>
+      </div>
+    </>
+  )
+}
+
+export default App
